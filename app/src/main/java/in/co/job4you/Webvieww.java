4363482c69f80c2efd0149eef642a452d6ffff30
Neha <nehaappdev@gmail.com>
2017-12-10 23:32:42 +0530

    Second Commit

--- app/src/main/java/in/co/job4you/Webvieww.java
+++ app/src/main/java/in/co/job4you/Webvieww.java
@@ -1,22 +1,25 @@
 package in.co.job4you;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class Webvieww extends AppCompatActivity {
-    WebView webview;
+    WebView webview;ProgressBar pb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        pb=(ProgressBar)findViewById(R.id.pb);
         webview=(WebView)findViewById(R.id.webvw);
         webview.getSettings().setJavaScriptEnabled(true); // enable javascript
 
@@ -38,6 +41,25 @@ public class Webvieww extends AppCompatActivity {
 
         webview .loadUrl(getIntent().getStringExtra("url"));
 
+        webview.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon)
+            {
+                // TODO show you progress image
+                super.onPageStarted(view, url, favicon);
+                webview.setVisibility(View.GONE);
+                pb.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url)
+            {
+                // TODO hide your progress image
+                super.onPageFinished(view, url);
+                webview.setVisibility(View.VISIBLE);
+                pb.setVisibility(View.GONE);
+            }
+        });
     }
 
 }
